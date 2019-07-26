bf1dec3f93a26e6fedc7ef2856c9582f6e7d288f
YordanNaumov <yordannaumov99@gmail.com>
2019-07-26 20:43:35 +0300

    first exercise

---
+++ RegEx/1. Furniture/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace _1.Furniture
+{
+    class Program
+    {
+        static void Main()
+        {
+            string command = Console.ReadLine();
+            List<string> boughtItems = new List<string>();
+            
+            while (command != "Purchase")
+            {
+                Regex regex = new Regex(); 
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}

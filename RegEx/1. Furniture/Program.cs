454ac1997641dfc5a5eb938c7515d8ca84720f1a
YordanNaumov <yordannaumov99@gmail.com>
2019-07-27 10:01:45 +0300

    Task 1. finished

--- RegEx/1. Furniture/Program.cs
+++ RegEx/1. Furniture/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-
 namespace _1.Furniture
 {
     class Program
@@ -11,13 +10,29 @@ namespace _1.Furniture
         {
             string command = Console.ReadLine();
             List<string> boughtItems = new List<string>();
+            double sumTotal = 0;
             
             while (command != "Purchase")
             {
-                Regex regex = new Regex(>> ([\w\s] +) <<); 
+                Regex regex = new Regex(@">>(.*)<<([\d]+\.[\d]*|[\d]+)!([\d]+)");//>>([\w\s]+)<<([\d]+\.[\d]*|[\d]+)!([\d]+)
+                var matches = regex.Matches(command);
 
+                foreach (Match match in matches)
+                {
+                    boughtItems.Add(match.Groups[1].ToString());
+                    double sum = Convert.ToDouble(match.Groups[2].ToString());
+                    int quantity = int.Parse(match.Groups[3].ToString());
+                    sumTotal += sum * Convert.ToDouble(quantity);
+                }
                 command = Console.ReadLine();
             }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var item in boughtItems)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total money spend: {sumTotal:f2}");
         }
     }
 }

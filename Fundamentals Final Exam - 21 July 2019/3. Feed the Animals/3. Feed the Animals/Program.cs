8a6c159ce940fee6a4a4b1a54aa834b466d5997d
YordanNaumov <yordannaumov99@gmail.com>
2019-07-31 22:35:43 +0300

    Task 3. started

---
+++ Fundamentals Final Exam - 21 July 2019/3. Feed the Animals/3. Feed the Animals/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _3.Feed_the_Animals
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> animalAndFood = new Dictionary<string, int>();
+            Dictionary<string, int> locationAndAnimals = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(':').ToArray();
+
+                if (command[0] == "Last Info")
+                {
+                    break;
+                }
+                else if (command[0] == "Add")
+                {
+                    string name = command[1];
+                    int foodLimit = int.Parse(command[2]);
+                    string area = command[3];
+
+                    if (animalAndFood.ContainsKey(name))
+                    {
+                        animalAndFood[name] += foodLimit;
+                    }
+                    else
+                    {
+                        animalAndFood[name] = foodLimit;
+                    }
+                }
+                else //Feed
+                {
+                    
+                }
+            }
+            
+        }
+    }
+}
